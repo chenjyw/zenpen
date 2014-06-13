1b213c2e7adb4b409ff4d0824013269b6d8d2e68
JC <chenjyw@gmail.com>
2014-06-12 18:08:06 -0700

    clean up

--- js/zenpen.js
+++ js/zenpen.js
@@ -1,208 +1,16 @@
-// Utility functions
-
-String.prototype.trim = function(){ return this.replace(/^\s+|\s+$/g, ''); };
-
-function get_text(el) {
-    var ret = " ";
-    var length = el.childNodes.length;
-    for(var i = 0; i < length; i++) {
-        var node = el.childNodes[i];
-        if(node.nodeType != 8) {
-
-        	if ( node.nodeType != 1 ) {
-        		// Strip white space.
-        		ret += node.nodeValue;
-        	} else {
-        		ret += get_text( node );
-        	}
-        }
-    }
-    return ret.trim();
-}
-
-// UI
-var zenPenUI = (function() {
-
-	// Base elements
-	var body, article, uiContainer, overlay, aboutButton, descriptionModal;
-
-	// Buttons
-	var screenSizeElement, colorLayoutElement, targetElement, saveElement;
-
-	// Work Counter
-	var wordCountValue, wordCountBox, wordCountElement, wordCounter, wordCounterProgress;
-	
-	//save support
-	var supportSave, saveFormat, textToWrite;
-	
-	var expandScreenIcon = '&#xe000;';
-	var shrinkScreenIcon = '&#xe004;';
-
-	var darkLayout = false;
-
-	function init() {
-		
-		bindElements();
-		
-	}
-
-	function bindElements() {
-
-		// Body element for light/dark styles
-		body = document.body;
-
-		uiContainer = document.querySelector( '.ui' );
-
-		// UI element for color flip
-		colorLayoutElement = document.querySelector( '.color-flip' );
-		colorLayoutElement.onclick = onColorLayoutClick;
-		
-		// Overlay when modals are active
-		overlay = document.querySelector( '.overlay' );
-		overlay.onclick = onOverlayClick;
-
-		article = document.querySelector( '.content' );
-		article.onkeyup = onArticleKeyUp;
-
-		descriptionModal = overlay.querySelector( '.description' );
-		
-	}
-
-	function selectFormat( e ) {
-		
-		if ( document.querySelectorAll('span.activesave').length > 0 ) {
-			document.querySelector('span.activesave').className = '';
-		}
-		
-		document.querySelector('.saveoverlay h1').style.cssText = '';
-		
-		var targ;
-		if (!e) var e = window.event;
-		if (e.target) targ = e.target;
-		else if (e.srcElement) targ = e.srcElement;
-		
-		// defeat Safari bug
-		if (targ.nodeType == 3) {
-			targ = targ.parentNode;
-		}
-			
-		targ.className ='activesave';
-		
-		
-		var header = document.querySelector('header.header');
-		var headerText = header.innerHTML.replace(/(\r\n|\n|\r)/gm,"") + "\n";
-		
-		var body = document.querySelector('article.content');
-		var bodyText = body.innerHTML;
-			
-		textToWrite = formatText(saveFormat,headerText,bodyText);
-		
-		var textArea = document.querySelector('.hiddentextbox');
-		textArea.value = textToWrite;
-		textArea.focus();
-		textArea.select();
-
-	}
-
-	function formatText( type, header, body ) {
-		
-		var text;
-		switch( type ) {
-
-			case 'html':
-				header = "<h1>" + header + "</h1>";
-				text = header + body;
-				text = text.replace(/\t/g, '');
-			break;
-
-			case 'markdown':
-				header = header.replace(/\t/g, '');
-				header = header.replace(/\n$/, '');
-				header = "#" + header + "#";
-			
-				text = body.replace(/\t/g, '');
-			
-				text = text.replace(/<b>|<\/b>/g,"**")
-					.replace(/\r\n+|\r+|\n+|\t+/ig,"")
-					.replace(/<i>|<\/i>/g,"_")
-					.replace(/<blockquote>/g,"> ")
-					.replace(/<\/blockquote>/g,"")
-					.replace(/<p>|<\/p>/gi,"\n")
-					.replace(/<br>/g,"\n");
-				
-				var links = text.match(/<a href="(.+)">(.+)<\/a>/gi);
-				
-                                if (links !== null) {
-                                        for ( var i = 0; i<links.length; i++ ) {
-                                                var tmpparent = document.createElement('div');
-                                                tmpparent.innerHTML = links[i];
-                                                
-                                                var tmp = tmpparent.firstChild;
-                                                
-                                                var href = tmp.getAttribute('href');
-                                                var linktext = tmp.textContent || tmp.innerText || "";
-                                                
-                                                text = text.replace(links[i],'['+linktext+']('+href+')');
-                                        }
-                                }
-				
-				text = header +"\n\n"+ text;
-			break;
-
-			case 'plain':
-				header = header.replace(/\t/g, '');
-			
-				var tmp = document.createElement('div');
-				tmp.innerHTML = body;
-				text = tmp.textContent || tmp.innerText || "";
-				
-				text = text.replace(/\t/g, '')
-					.replace(/\n{3}/g,"\n")
-					.replace(/\n/,""); //replace the opening line break
-				
-				text = header + text;
-			break;
-			default:
-			break;
-		}
-		
-		return text;
-	}
-
-	function onOverlayClick( event ) {
-
-		if ( event.target.className === "overlay" ) {
-			removeOverlay();
-		}
-	}
-
-	function removeOverlay() {
-		
-		overlay.style.display = "none";
-		descriptionModal.style.display = "none";
-		saveModal.style.display = "none";
-		
-		if ( document.querySelectorAll('span.activesave' ).length > 0) {
-			document.querySelector('span.activesave').className = '';
-		}
-
-	}
-
-	return {
-		init: init
-	}
-
-})();
 
 
 // Editor 
-var zenPen = (function() {
+var ZenPen = (function() {
 
 	// Editor elements
 	var headerField, contentField, cleanSlate, lastType, currentNodeList, savedSelection;
 
 	// Editor Bubble elements
-	var textOptions, optionsBox, boldButton, italicButton, quoteButton, urlButton, urlInput;
+	var textOptions, optionsBox, urlButton, urlInput;
+
+	// Editor UI
+	var body, article, overlay;
 
 	var composing;
 
@@ -210,13 +18,6 @@ var zenPen = (function() {
 
 		composing = false;
 		bindElements();
-
-		// Set cursor position
-		var range = document.createRange();
-		var selection = window.getSelection();
-		range.setStart(headerField, 1);
-		selection.removeAllRanges();
-		selection.addRange(range);
 
 		createEventBindings();
 	}
@@ -256,20 +57,10 @@ var zenPen = (function() {
 
 	function bindElements() {
 
-		headerField = document.querySelector( '.header' );
-		contentField = document.querySelector( '.content' );
+		contentField = document.querySelector( '.with-toolbar' );
 		textOptions = document.querySelector( '.text-options' );
 
 		optionsBox = textOptions.querySelector( '.options' );
-
-		boldButton = textOptions.querySelector( '.bold' );
-		boldButton.onclick = onBoldClick;
-
-		italicButton = textOptions.querySelector( '.italic' );
-		italicButton.onclick = onItalicClick;
-
-		quoteButton = textOptions.querySelector( '.quote' );
-		quoteButton.onclick = onQuoteClick;
 
 		urlButton = textOptions.querySelector( '.url' );
 		urlButton.onmousedown = onUrlClick;
@@ -277,6 +68,15 @@ var zenPen = (function() {
 		urlInput = textOptions.querySelector( '.url-input' );
 		urlInput.onblur = onUrlInputBlur;
 		urlInput.onkeydown = onUrlInputKeyDown;
+
+		// Body element for light/dark styles
+		body = document.body;
+		
+		// Overlay when modals are active
+		overlay = document.querySelector( '.overlay' );
+		overlay.onclick = onOverlayClick;
+
+		article = document.querySelector( '.with-toolbar' );		
 	}
 
 	function checkTextHighlighting( event ) {
@@ -331,24 +131,6 @@ var zenPen = (function() {
 		// It would be possible to use classList here, but I feel that the
 		// browser support isn't quite there, and this functionality doesn't
 		// warrent a shim.
-
-		if ( hasNode( currentNodeList, 'B') ) {
-			boldButton.className = "bold active"
-		} else {
-			boldButton.className = "bold"
-		}
-
-		if ( hasNode( currentNodeList, 'I') ) {
-			italicButton.className = "italic active"
-		} else {
-			italicButton.className = "italic"
-		}
-
-		if ( hasNode( currentNodeList, 'BLOCKQUOTE') ) {
-			quoteButton.className = "quote active"
-		} else {
-			quoteButton.className = "quote"
-		}
 
 		if ( hasNode( currentNodeList, 'A') ) {
 			urlButton.className = "url useicons active"
@@ -473,7 +255,6 @@ var zenPen = (function() {
 	}
 
 	function applyURL( url ) {
-
 		rehighlightLastSelection();
 
 		// Unlink any current links
@@ -503,6 +284,25 @@ var zenPen = (function() {
 	function onCompositionEnd (event) {
 		composing = false;
 	}
+
+	function onOverlayClick( event ) {
+
+		if ( event.target.className === "overlay" ) {
+			removeOverlay();
+		}
+	}
+
+	function removeOverlay() {
+		
+		overlay.style.display = "none";
+		descriptionModal.style.display = "none";
+		saveModal.style.display = "none";
+		
+		if ( document.querySelectorAll('span.activesave' ).length > 0) {
+			document.querySelector('span.activesave').className = '';
+		}
+
+	}	
 
 	return {
 		init: init,
